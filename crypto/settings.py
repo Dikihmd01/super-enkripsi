63a6452a62e4ce4d7fe98285992ee7e13da79b88
Dikihmd01 <dikihamdani.id@gmail.com>
2022-04-12 09:27:50 +0700

    update settings

--- crypto/settings.py
+++ crypto/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-1ig#3!zlr7=b5i7cc!tze@h-d6+9s8z$wv0pygsd#ntu!tu)k-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['kripto-dikihamdani.herokuapp.com']
 
 
 # Application definition

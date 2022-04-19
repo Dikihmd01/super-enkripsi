c8ec8a9f4f05cd71c86f89171ec397395c61dce6
Dikihmd01 <dikihamdani.id@gmail.com>
2022-04-19 20:38:26 +0700

    update algoritma

--- cryptoapp/forms.py
+++ cryptoapp/forms.py
@@ -1,15 +1,36 @@
 from django import forms
 
 class CryptoForm(forms.Form):
-    message = forms.CharField(label="Masukkan pesan",
+    message = forms.CharField(label="Pesan",
+                                error_messages= {
+                                    'required': 'Pesan harus diisi'
+                                },
                                 widget=forms.Textarea(
                                     attrs={
                                         'class': 'form-control',
                                         'rows': 2,
-                                        'cols': 3
+                                        'cols': 3,
+                                        'placeholder': 'Masukkan pesan'
                                     }
                                 ))
-    key = forms.CharField(label="Masukan kunci",
+
+    key = forms.CharField(label="Kunci Vigenere Cipher",
                             max_length=250, widget=forms.TextInput(
-                                attrs={'class': 'form-control'}
+                                attrs={
+                                    'class': 'form-control',
+                                    'placeholder': 'Masukkan kunci berupa kata',
+                                    'required': 'Kunci vigenere harus diisi'
+                                }
+                            ))
+
+    key_rf = forms.IntegerField(label="Kunci Rail Fence",
+                            error_messages= {
+                                'invalid': 'Kunci harus diisi dengan angka dari 0-9!',
+                                'required': 'Kunci Rail Fence harus diisi'
+                            },
+                            widget=forms.TextInput(
+                                attrs={
+                                    'class': 'form-control',
+                                    'placeholder': 'Hanya berupa angka dari 2-9'
+                                }
                             ))
--- cryptoapp/views.py
+++ cryptoapp/views.py
@@ -1,9 +1,10 @@
 from ast import keyword
+from django.forms import ValidationError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from .forms import CryptoForm
 
-alphabets = "abcdefghijklmnopqrstuvwxyz"
+alphabets = "ABCDEFGHIJKLMNOPQRSTU"
 
 word_to_index = dict(zip(alphabets, range(len(alphabets))))
 index_to_word = dict(zip(range(len(alphabets)), alphabets))
@@ -19,8 +20,8 @@ def generate_key(message, key):
 
 def encrypt_vigenere(message, key):
     encrypted = ""
-    message = message.lower()
-    key = key.lower()
+    message = message.upper()
+    key = key.upper()
     split_message = [
         message[i : i + len(key)] for i in range(0, len(message), len(key))
     ]
@@ -34,16 +35,16 @@ def encrypt_vigenere(message, key):
                 number = (word_to_index[letter] + word_to_index[key[i]]) % len(alphabets)
                 encrypted += index_to_word[number]
                 i += 1
-            else:
-                encrypted += letter
+            # else:
+            #     encrypted += letter
 
     return encrypted
 
 
 def decrypt_vigenere(cipher, key):
     decrypted = ""
-    cipher = cipher.lower()
-    key = key.lower()
+    cipher = cipher.upper()
+    key = key.upper()
     split_encrypted = [
         cipher[i : i + len(key)] for i in range(0, len(cipher), len(key))
     ]
@@ -56,62 +57,145 @@ def decrypt_vigenere(cipher, key):
                 number = (word_to_index[letter] - word_to_index[key[i]]) % len(alphabets)
                 decrypted += index_to_word[number]
                 i += 1
-            else:
-                decrypted += letter
+            # else:
+            #     decrypted += letter
 
     return decrypted
+
+def encrypt_rail_fence(text, key):
+    text = text.replace(" ", "")
+    text = text.upper()
+    r = [['\n' for i in range(len(text))] for j in range(key)]
+    dir_down = False
+    row, col = 0, 0
+
+    for i in range(len(text)):
+        if (row == 0) or (row == key - 1):
+            dir_down = not dir_down
+
+        r[row][col] = text[i]
+        col += 1
+
+        if dir_down:
+            row += 1
+        else:
+            row -= 1
+
+    result = []
+
+    for i in range(key):
+        for j in range(len(text)):
+            if r[i][j] != '\n':
+                result.append(r[i][j])
+    return("" . join(result))
+
+def decrypt_rail_fence(cipher, key):
+    r = [['\n' for i in range(len(cipher))] for j in range(key)] 
+    dir_down = None
+    row, col = 0, 0
+
+    for i in range(len(cipher)):
+        if row == 0:
+            dir_down = True
+        if row == key - 1:
+            dir_down = False
+
+        r[row][col] = '*'
+        col += 1
+
+        if dir_down:
+            row += 1
+        else:
+            row -= 1
+
+    index = 0
+    for i in range(key):
+        for j in range(len(cipher)):
+            if ((r[i][j] == '*') and
+            (index < len(cipher))):
+                r[i][j] = cipher[index]
+                index += 1
+
+    result = []
+    row, col = 0, 0
+
+    for i in range(len(cipher)):
+        if row == 0:
+            dir_down = True
+        if row == key-1:
+            dir_down = False
+        if (r[row][col] != '*'):
+            result.append(r[row][col]) 
+            col += 1
+        if dir_down:
+            row += 1
+        else:
+            row -= 1
+    return("".join(result))
 
 # caesar cipher
-def encrypt_caesar(message, shift=3):
-    cipher = ""
-    message = message.lower()
-    for word in message:
-        if word in list(alphabets):
-            number = (word_to_index[word] + shift) % len(word_to_index)
-            word = index_to_word[number]
-            cipher += word
-        else:
-            cipher += word
-    return cipher
+# def encrypt_caesar(message, shift):
+#     cipher = ""
+#     message = message.upper()
+#     for word in message:
+#         if word in list(alphabets):
+#             number = (word_to_index[word] + int(shift)) % len(word_to_index)
+#             word = index_to_word[number]
+#             cipher += word
+#         else:
+#             cipher += word
+#     return cipher
 
-def decrypt_caesar(cipher, shift=3):
-    decrypted = ""
-    cipher = cipher.lower()
-    for word in cipher:
-        if word in list(alphabets):
-            number = (word_to_index[word] - shift) % len(word_to_index)
-            word = index_to_word[number]
-            decrypted += word
-        else:
-            decrypted += word
-    return decrypted
+# def decrypt_caesar(cipher, shift):
+#     decrypted = ""
+#     cipher = cipher.upper()
+#     for word in cipher:
+#         if word in list(alphabets):
+#             number = (word_to_index[word] - shift) % len(word_to_index)
+#             word = index_to_word[number]
+#             decrypted += word
+#         else:
+#             decrypted += word
+#     return decrypted
+
+# def encrypt_rail_fence(message, rails):
+#     message = message.upper()
 
 def index(request):
     data = {}
+    error_msg = False
     if request.method == 'POST':
         form = CryptoForm(request.POST)
         if form.is_valid():
             
-            msg = request.POST['message'].lower()
-            message = msg.replace(" ", "")
-            key1 = request.POST['key'].lower()
-            keyword = key1.replace(" ", "")
-            key = generate_key(msg, keyword)
-            vigenere_encrypt_text = encrypt_vigenere(msg, key1)
-            vigenere_decrypt_text = decrypt_vigenere(vigenere_encrypt_text, key1)
+            message = request.POST['message'].upper().replace(" ", "")
+            keyword = request.POST['key'].upper()
+            key = generate_key(message, keyword)
+            rails = request.POST['key_rf']
+            
+            if int(rails) < 2:
+                rails = 2
 
-            final_encrypt_text = encrypt_caesar(vigenere_encrypt_text, shift=3)
-            final_decrypt_text = decrypt_caesar(final_encrypt_text, shift=3)
+
+            vigenere_encrypt_text = encrypt_vigenere(message, keyword)
+            vigenere_decrypt_text = decrypt_vigenere(vigenere_encrypt_text, keyword)
+
+            final_encrypt_text = encrypt_rail_fence(vigenere_encrypt_text, int(rails))
+            final_decrypt_text1 = decrypt_rail_fence(final_encrypt_text, int(rails))
+            final_decrypt_text2 = decrypt_vigenere(final_decrypt_text1, keyword)
             # form.cleaned_data()
 
             data = {
-                'message': msg,
-                'keyword': key1,
+                'message': message,
+                'keyword': keyword,
                 'key': key,
+                'shift': rails,
                 'vigenere_encrypt_text': vigenere_encrypt_text,
                 'vigenere_decrypt_text': vigenere_decrypt_text,
                 'final_encrypt_text': final_encrypt_text,
-                'final_decrypt_text': final_decrypt_text
+                'final_decrypt_text1': final_decrypt_text1,
+                'final_decrypt_text2': final_decrypt_text2,
+                'error_msg': error_msg
             }
             form = CryptoForm()
     else:
